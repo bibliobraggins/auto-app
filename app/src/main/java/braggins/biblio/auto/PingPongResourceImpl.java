da88779fc7068a1d74c20ce55a698f1c9b7fd3ac
Chloe Law <bibiobraggins@gmail.com>
2023-06-23 10:52:30 -0600

    typo

--- app/src/main/java/braggins/biblio/auto/PingPongResourceImpl.java
+++ app/src/main/java/braggins/biblio/auto/PingPongResourceImpl.java
@@ -12,7 +12,7 @@ public class PingPongResourceImpl implements PingPongApi {
 
     @Override
     public String ping() {
-        log.info("received ping, relying pong");
+        log.info("received ping, replying pong");
         jdbi.withHandle(h -> {
             final var execute = h.execute("select 1");
             log.info("db query response code {}", execute);
